ec3a20f74afc091c9908b207a0561fc326c4b98f
unnati-bhatia <pikku2811@gmail.com>
2022-12-03 04:23:29 +0530

    Front page of website

--- beta-19/pages/_app.tsx
+++ beta-19/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
